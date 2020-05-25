8b8a5270941fc7fb168b9f0c2f75f4aad415b26e
Daniel Reszka <dannn.r@gmail.com>
2020-05-25 14:16:19 +0200

    implement graphql example

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,35 @@
-import Link from 'next/link'
+import useSWR from "swr";
 
-export default function Home() {
+const GET_USERS = /* GraphQL */ `
+  {
+    users {
+      name
+    }
+  }
+`;
+
+const fetcher = (query) =>
+  fetch("/api/graphql", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ query }),
+  })
+    .then((res) => res.json())
+    .then((json) => json.data);
+
+export default function Index() {
+  const { data, error } = useSWR(GET_USERS, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
+  const { users } = data;
+
   return (
-    <ul>
-      <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
-  )
+    <div>
+      {users.map((user, i) => (
+        <div key={i}>{user.name}</div>
+      ))}
+    </div>
+  );
 }
--- server.js
+++ server.js
@@ -1,28 +1,51 @@
-const express = require('express')
-const next = require('next')
+const {
+  ApolloServer,
+  makeExecutableSchema,
+  gql,
+} = require("apollo-server-express");
+const express = require("express");
+const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+  }
+  type User {
+    name: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users() {
+      return [{ name: "Nextjs" }];
+    },
+  },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 app.prepare().then(() => {
-  const server = express()
+  const path = "/api";
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/a', req.query)
-  })
+  const server = express();
 
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
+  const apolloServer = new ApolloServer({ schema });
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+  apolloServer.applyMiddleware({ app: server, path });
+
+  server.all("*", (req, res) => handle(req, res));
 
   server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
